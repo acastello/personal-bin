fab57e1c4d85c2471e751e4d5eb2b2686e876a1b
Alejandro Castelló <a.castello.pas@gmail.com>
2018-12-25 20:48:58 +0100

    Create vim-encrypted diary tool

--- colors.sh
+++ colors.sh
@@ -1,11 +1,26 @@
-#!/bin/bash
+#!/bin/bash --
 
-ARGV=( "$@" )
+ARGV=()
+NARGV=$#
+EXTENDED=
 
-if [ $# -gt 0 ] ; then
+while [ x"$1" != x ] ; do
+    case "$1" in 
+    -a)
+        EXTENDED=y
+        NARGV=$(($NARGV - 1))
+        ;;
+     *)
+         ARGV+="$1"
+         ;;
+    esac
+    shift
+done
+
+if [ $NARGV -gt 0 ] ; then
     BARWIDTH=0
     for i in {0..8} ; do
-        FGNAMES[$i]="${ARGV[$(($i % $#))]}"
+        FGNAMES[$i]="${ARGV[$(($i % $NARGV))]}"
         BARWIDTH=$(($BARWIDTH + ${#FGNAMES[$i]} + 2))
     done
 else
@@ -23,6 +38,7 @@ else
 fi
 BGNAMES=('DFT' 'BLK' 'RED' 'GRN' 'YEL' 'BLU' 'MAG' 'CYN' 'WHT')
         
+
 BAR=$(printf "─%.0s" $(seq 1 $BARWIDTH))
 echo "     ┌─${BAR}─┐"
 for b in $(seq 0 8); do
@@ -36,15 +52,34 @@ for b in $(seq 0 8); do
     done
     echo -en "\033[0m │"
 
-    echo -en "\033[0m\n\033[0m     │ "
+    if [ $b -gt 0 ]; then
+        echo -en "\033[0m\n\033[0m $(($b+29))  │ "
+    else
+        echo -en "\033[0m\n\033[0m     │ "
+    fi
     for f in $(seq 0 8); do
       echo -en "\033[${bg}m\033[1;$(($f+29))m ${FGNAMES[$f]} "
     done
     echo -en "\033[0m │"
         echo -e "\033[0m"
+    if [ $b -gt 0 ] && [ x$EXTENDED = xy ] ; then 
+        bg=$(($b+99))
+        echo -en "\033[0m     │ "
+        for f in $(seq 0 8); do
+          echo -en "\033[${bg}m\033[$(($f+29))m ${FGNAMES[$f]} "
+        done
+        echo -en "\033[0m │"
 
-  if [ "$b" -lt 8 ]; then
-    echo "     ├─${BAR}─┤"
-  fi
+        echo -en "\033[0m\n\033[0m     │ "
+        for f in $(seq 0 8); do
+          echo -en "\033[${bg}m\033[1;$(($f+29))m ${FGNAMES[$f]} "
+        done
+        echo -en "\033[0m │"
+            echo -e "\033[0m"
+    fi
+
+    if [ "$b" -lt 8 ]; then
+      echo "     ├─${BAR}─┤"
+    fi
 done
 echo "     └─${BAR}─┘"

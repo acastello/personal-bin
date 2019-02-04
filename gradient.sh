552a1f346a5d708e15fa7cd81b66a1ddbbfc0d63
Alejandro Castelló <a.castello.pas@gmail.com>
2019-02-04 01:23:37 +0100

    fix mtimefind, add stuff

---
+++ gradient.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+CHARS=( '█   ' 
+        '▓█  ' 
+        '▒▓█ ' 
+        '░▒▓█' 
+        ' ░▒▓' 
+        '  ░▒' 
+        '   ░' )
+I=0
+
+while read -r line || [ -n "$line" ] ; do
+    echo -e "\e[1m${CHARS[$I]} \e[21m $line"
+    I=$((($I + 1) % ${#CHARS[@]}))
+done

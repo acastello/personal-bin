ef76c7630d3723b7d729b9c1801369b71df0603d
Alejandro Castello <alejandro.castello@freiheit.com>
2019-03-10 10:59:47 +0100

    Allow overriding args

--- swagger-gen.sh
+++ swagger-gen.sh
@@ -3,11 +3,21 @@
 set -eu
 
 [[ -f "$1" ]]
+in="$1"
+target="$2"
+
+shift 2
+
+if [ $# -gt 0 ]; then
+    args=("$@")
+else
+    args=("-l" "html2")
+fi
 
 DIR="$(mktemp -d)"
 
 trap "rm -rf \"$DIR\"" EXIT
 
-swagger-codegen generate -i "$1" -l html2 -o "$DIR"
-mv -fv "$DIR/index.html" "$2"
+swagger-codegen generate -i "$in" "${args[@]}" -o "$DIR"
+mv -fv "$DIR/index.html" "$target"
 
